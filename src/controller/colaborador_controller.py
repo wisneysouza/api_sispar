f2eff0b3c539f3047922570e0f2ae1ba834ff209
wisneysouza <wisney.souza@gmail.com>
2025-05-09 01:30:27 -0300

    complementando os arquivos colaborador_controller e reembolso_model

--- src/controller/colaborador_controller.py
+++ src/controller/colaborador_controller.py
@@ -4,8 +4,11 @@ from flask import Blueprint, request, jsonify
 #jsonify e um retorno, responsavel por transformar os dados em json e envia o status cold ex 404, 504 e etc.
 from src.model.colaborador_model import Colaborador
 from src.model import db
-import src.security.security
+import src.security.security import hash_senha, checar_senha
 from flasgger import swag_from
+
+#request -> trabalha com as requisições. Pega o conteúdo da requisição
+# jsonify -> Trabalha com as respostas. Converte um dado em Json
 
 bp_colaborador = Blueprint('colaborador', __name__, url_prefix= '/colaborador')
 
@@ -28,19 +31,68 @@ def cadastrar_novo_colaborador():
 
     novo_colaborador = Colaborador(
 
-        nome= dados_requisicao['nome'],
+        nome= dados_requisicao['nome'], # Pegue do json o valor relacionado a chave nome
         email=dados_requisicao['email'],
-        senha= dados_requisicao['senha'],
+        senha= hash_senha(dados_requisicao['senha']),
         cargo= dados_requisicao['cargo'],
         salario= dados_requisicao['salario']
     )
 
-    #INSERT INTO tb_colaborador (nome, email, senha, cargo, salario )
+    #INSERT INTO tb_colaborador (nome, email, senha, cargo, salario ) VALUES (VALOR1, VALOR2, VALOR3, VALOR4, VALOR5)
     db.session.add(novo_colaborador)
-    db.session.commit()
+    db.session.commit()  # Essa linha executa a query
 
     return jsonify( {'mensagem': 'Colaborador cadastrado com sucesso'}), 201
-
-
 #sinaliza que os dados enviados
-# endereco
+
+# Endereco/colaborador/atualizar/1
+@bp_colaborador.route('/atualizar/<int:id_colaborador>', methods=['PUT'])
+def atualizar_dados_do_colaborador(id_colaborador):
+
+    dados_requisição = request.get_json()
+
+    for colaborador in dados:
+        if colaborador['id'] == id_colaborador:
+            colaborador_encontrado = colaborador
+            break
+    if 'nome' in dados_requisição:
+        colaborador_encontrado ['nome'] = dados_requisição['nome']
+    if 'cargo' in dados_encontrado['cargo'] = dados_requisição['cargo']
+
+    returnjsonify({'mensagem': 'Dados do colaborador atualizados com sucesso'}), 200
+
+    @bp_colaborador.route('/login', methods=['POST'])
+    def login():
+
+        dados_requisição = request.get_json()
+
+        email= dados_requisição.get('email')
+        senha = dados_requisição.get('senha')
+
+        if not email or not senha:
+            return jsonify({'mensagem': 'Todos os dados precisam ser preenchidos'}), 400
+        
+
+        #SELECT * FROM [TABELA]
+        colaborador = db.session.execute(
+            db.select(Colaborador).Where(Colaborador.email == email)
+        ).scalar() # -> A linha de informação ou none
+
+
+        print('*'*100)
+        print(f'dados:{colaborador} é do tipo {type(colaborador)}')
+        print('*'*100)
+
+        if not colaborador
+            return jsonify({'mensagem' : 'Usuario não encontrado'}),404
+        
+        colaborador = colaborador.to_dict()
+
+        print('*'*100)
+        print(f'dado: {colaborador} é do tipo {type(colaborador)}')
+        print('*'*100)
+
+        if email == colaborador.get('email') and checar_senha(senha, colaborador.get('senha')):
+            return jsonify({'mensagem': 'Login realizado com sucesso'}), 200
+        else:
+            return jsonify({'mensagem': ' Credenciais invalidas'}),400
--- src/model/reembolso_model.py
+++ src/model/reembolso_model.py
@@ -1,5 +1,5 @@
 from src.model import db
-from sqlalchemy.schema import Column
+from sqlalchemy.schema import Column, ForeignKey
 from sqlalchemy.types import Integer, String, DECIMAL, DATE
 from sqlalchemy import func
 
@@ -8,20 +8,39 @@ class Reembolso(db.Model): # Interpreta que essa classe vai ser o modelo para a 
     #---------------------ATRIBUTOS--------------------------------
     id = Column(Integer, primary_key True, autoincremente=True)
     colaborador = Column(String(150), nullabel=False)
-    empresa = Column(String(50))#os nomes serão abreviados, por isso 50 caracteres
-    num_prestacao = Column(Integer)
+    empresa = Column(String(50), nullable=False)#os nomes serão abreviados, por isso 50 caracteres
+    num_prestacao = Column(Integer, nullable=False)
     descricao = Column(String(255))
-    data = Column(DATE, server_default=func.current_date())
-    tipo_reembolso = Column(String(35))
-    centro_custo = Column(String(50))
+    data = Column(DATE, server_default=func.current_date(), nullable=False)
+    tipo_reembolso = Column(String(35), nullable=False)
+    centro_custo = Column(String(50), nullable = False)
     ordem_interna = Column(String(50))
     divisao = Column(String(50))
     pep =Column(String(50))
-    moeda = Column(String(20))
+    moeda = Column(String(20), nullable=False)
     distancia_km = Column(String(50))
     valor_km = Column(String(50))
-    valor_faturado = Column(DECIMAL(10,2))#9999999999.11
+    valor_faturado = Column(DECIMAL(10,2), nullable=False)#9999999999.11
     despesa = Column(DECIMAL(10,2))
+    id_colaborador = Column(Integer, ForeignKey(Column='colaborador.id'))
+    status = Column(String(25))
 
     #----------------------Metodo Construtor-------------------------------------
-    def __init__(self):
+    def __init__(self, colaborador, empresa, num_prestação, descricao, data, tipo_reembolso, centro_custo, ordem_interna, divisao, pep, moeda, distancia_km, valor_km, valor_faturado, despesa, id_colabolador, status='Em analise'):
+        self.colaborador = colaborador
+        self.empresa = empresa
+        self.num_prestacao = num_prestação
+        self.descricao = descricao
+        self.data = data
+        self.tipo_reembolso = tipo_reembolso
+        self.centro_custo = centro_custo
+        self.ordem_interna = ordem_interna
+        self.divisao = divisao
+        self.pep = pep
+        self.moeda = moeda
+        self.distancia_km = distancia_km
+        self.valor_km = valor_km
+        self.valor_faturado = valor_faturado
+        self.despesa = despesa
+        self.id_colaborador = id_colabolador
+        self.status = status
