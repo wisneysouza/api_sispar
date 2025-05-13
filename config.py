85a0b71c3469bc1463ac5fce1607d243d399957f
wisneysouza <wisney.souza@gmail.com>
2025-05-13 15:18:23 -0300

    alterando config

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv # Traz a funçõa para carregar as variaveis de a
 load_dotenv() # Carrega as variaveis de ambiente para este arquivo
 
 class Config():
-   SQLALCHEMY_DATABASE_URI = 'mysql://root:1990@localhost:3306/sispar_t3'
+   SQLALCHEMY_DATABASE_URI = environ.get('URL_DATABASE_PROD')
    #SQLALCHEMY_DATABASE_URI = environ.get("URL_DATABASE_PROD")
    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
